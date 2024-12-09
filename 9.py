656882170418681bc58fd191d8511ca2e0c385b7
cumberjie <26396320+cumberjie@users.noreply.github.com>
2024-12-10 03:14:08 +0800

    Update 9.py

--- 9.py
+++ 9.py
@@ -5,7 +5,7 @@ with open("9.m3u", "r", encoding="utf-8") as file:
     content = file.read()
 
 # 替换IPv6地址
-updated_content = re.sub(r'\[.*?\]', '', content)
+updated_content = re.sub(r'\[.*?\]/', '', content)
 
 # 保存修改后的文件
 with open("99.m3u", "w", encoding="utf-8") as file:

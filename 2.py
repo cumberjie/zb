161658b6cfb5989a1e262c259713cc065eb97f4f
cumberjie <26396320+cumberjie@users.noreply.github.com>
2024-12-10 14:53:24 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -75,6 +75,6 @@ def sort_sources(input_file, output_file):
                 file.write(f"{name},{url}\n")
 
 if __name__ == "__main__":
-    input_file = "1.m3u"  # 输入文件
-    output_file = "2.m3u"  # 输出文件
+    input_file = "by.m3u"  # 输入文件
+    output_file = "byy.m3u"  # 输出文件
     sort_sources(input_file, output_file)
